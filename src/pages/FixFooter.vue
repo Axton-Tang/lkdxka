23240412cf962a20a4d92695d285ddeb74c00057
Axton <873948360@qq.com>
2020-05-10 13:25:19 +0800

    finish swiper-about

---
+++ src/pages/FixFooter.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="wrapper">
+        <div class="content-wrapper">
+            <div class="mail-wrapper">
+                <span class="iconfont">&#xe625;</span>
+                邮箱：lkdxka@163.com
+            </div>
+            <div class="row-line"></div>
+            <div class="detail-wrapper">
+                <div class="detail">
+                    <p>Copyright © 2018 - 2020 LKDXKA All Rights Reserved.</p>
+                    <p>湖南庠序文化传媒有限公司 &nbsp;&nbsp;觅漾工作室&nbsp;&nbsp;&nbsp;&nbsp;旗下品牌</p>
+                    <p class="pop-detail">开发者 ： 觅漾工作室技术部  &nbsp;
+                        Axton Tang
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HomeFooter',
+    }
+</script>
+
+<style lang="stylus" scoped>
+    .wrapper
+        position relative
+        float left
+        top .5rem
+        bottom 0
+        width 100%
+        height 0
+        padding-bottom 48%
+        background #2E2F33
+        color #6C6D6E
+        .content-wrapper
+            position relative
+            .mail-wrapper
+                position absolute
+                top .5rem
+                left .5rem
+                font-size .32rem
+            .row-line
+                position absolute
+                float left
+                top 1.25rem
+                width 90%
+                height 0
+                padding-bottom .02rem
+                margin 0 .45rem
+                background #6C6D6E
+                opacity .8
+            .detail-wrapper
+                position absolute
+                float left
+                width 100%
+                margin-top 1.5rem
+                .detail
+                    position flex
+                    text-align center
+                    font-size .26rem
+                    height .6rem
+                    line-height .6rem
+</style>
--- src/pages/swiper/About.vue
+++ src/pages/swiper/About.vue
@@ -1,21 +1,145 @@
 <template>
-    <div class="wrapper">
-        <router-link to="/">
-            <fix-header></fix-header>
-        </router-link>
+    <div>
+        <div class="wrapper">
+            <router-link to="/">
+                <fix-header></fix-header>
+            </router-link>
+            <div class="develop-wrapper">
+                <img class="develop-content" src="https://lkdxka.oss-cn-shenzhen.aliyuncs.com/home/%E5%85%B3%E4%BA%8E%E6%88%91%E4%BB%AC/%E5%8F%91%E5%B1%95%E5%8E%86%E7%A8%8B.png">
+            </div>
+            <div class="title-wrapper">
+                <div class="iconfont">&#xe6b5;</div>
+                <div class="title">部门介绍</div>
+                <div class="title-two">Introduction</div>
+            </div>
+            <div class="introduce-wrapper">
+                <div class="content">
+                    <div class="content-head">运营部</div>
+                    <p class="small-info">运营部下设策划组、官Q组、外联组。在这里可以尽情放飞你的idea！ </p>
+                </div>
+                <div class="img-wrapper">
+                    <img class="img-content" src="https://lkdxka.oss-cn-shenzhen.aliyuncs.com/home/%E5%85%B3%E4%BA%8E%E6%88%91%E4%BB%AC/%E8%BF%90%E8%90%A5%E9%83%A8.jpg">
+                </div>
+            </div>
+            <div class="line"></div>
+            <div class="introduce-wrapper">
+                <div class="content">
+                    <div class="content-head">编辑部</div>
+                    <p class="small-info">编辑部下设美术组、视频组、文案组。如果你文采奕奕、酷爱设计，这就是你绽放的舞台！ </p>
+                </div>
+                <div class="img-wrapper">
+                    <img class="img-content" src="https://lkdxka.oss-cn-shenzhen.aliyuncs.com/home/%E5%85%B3%E4%BA%8E%E6%88%91%E4%BB%AC/%E7%BC%96%E8%BE%91%E9%83%A8%20.jpeg">
+                </div>
+            </div>
+            <div class="line"></div>
+            <div class="introduce-wrapper">
+                <div class="content">
+                    <div class="content-head">技术部</div>
+                    <p class="small-info">技术部下设开发组和产品组。在技术大佬的帮助下，可以大胆的开发你自己的产品！</p>
+                </div>
+                <div class="img-wrapper">
+                    <img class="img-content" src="https://lkdxka.oss-cn-shenzhen.aliyuncs.com/home/%E5%85%B3%E4%BA%8E%E6%88%91%E4%BB%AC/%E6%8A%80%E6%9C%AF%E9%83%A8.jpg">
+                </div>
+            </div>
+        </div>
+        <fix-footer class="footer"></fix-footer>
     </div>
 </template>
 
 <script>
     import FixHeader from '../FixHeader'
+    import FixFooter from '../FixFooter'
+
     export default {
         name: 'About',
         components :{
-            FixHeader
+            FixHeader,
+            FixFooter
         }
     }
 </script>
 
 <style lang="stylus" scoped>
 
+    .wrapper
+        position relative
+        float left
+        overflow hidden
+        .develop-wrapper
+            position relative
+            margin-top 1.15rem
+            width 100%
+            height 0
+            padding-bottom 42.5556%
+            text-align center
+            .develop-content
+                width 93%
+                border-radius .26rem
+                margin-top .3rem
+                box-shadow 0 0 .07rem  #333333
+        .title-wrapper
+            position relative
+            float left
+            margin 0 .3rem
+            margin-top .3rem
+            height 1.2rem
+            line-height 1.2rem
+            font-size .38rem
+            width 100%
+            .iconfont
+                float left
+                margin-right .2rem
+                color #4CBCB0
+                font-size .38rem
+            .title
+                float left
+                font-weight 600
+                margin-right .2rem
+            .title-two
+                margin-top .06rem
+                font-size .32rem
+                color #bfc1c2
+                font-weight 600
+        .line
+            position relative
+            float left
+            width 92%
+            height .03rem
+            background #f1f1f1
+            margin-left .36rem
+        .introduce-wrapper
+            position relative
+            width 93%
+            height 0
+            padding-bottom 20%
+            float left
+            overflow hidden
+            margin .2rem 0 .2rem .3rem
+            .content
+                float left
+                width 68%
+                .content-head
+                    font-size .35rem
+                    letter-spacing .2rem
+                    font-weight 600
+                .small-info
+                    margin .2rem auto
+                    font-size .28rem
+                    color #9d9fa0
+                    line-height .32rem
+                    letter-spacing .05rem
+                    margin-right .2rem
+            .img-wrapper
+                float right
+                width 32%
+                height 100%
+                .img-content
+                    width 100%
+                    border-radius .16rem
+    .footer
+        position absolute
+        bottom 0
+        left 0
+        right 0
+        margin-top  .5rem
 </style>
